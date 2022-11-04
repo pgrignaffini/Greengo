7294ec3b61a80b84ba08c6662ae37505b6739e06
grignaffini <grignaffinipaolo@gmail.com>
2022-11-04 19:13:30 +0100

    trying to deploy contract from frontend

--- test/Campaign.ts
+++ test/Campaign.ts
@@ -1,5 +1,7 @@
 const { expect } = require("chai");
 import { time } from "@nomicfoundation/hardhat-network-helpers";
+const { ethers } = require("hardhat");
+const hre = require("hardhat");
 
 describe("Campaign contract", function () {
     async function getCampaign() {
@@ -8,7 +10,7 @@ describe("Campaign contract", function () {
         const Campaign = await hre.ethers.getContractFactory("Campaign");
         const amount = ethers.utils.parseUnits("0.01", "ether")
         const startDate = Math.floor(new Date().getTime() / 1000);
-        const endDate = startDate + 10000;
+        const endDate = startDate + 100000;
         // deploy campaign with 0.01 ether, 10 seconds duration
         const campaign = await Campaign.connect(creator).deploy("Test", amount, startDate, endDate);
         await campaign.deployed();
@@ -17,31 +19,33 @@ describe("Campaign contract", function () {
     }
 
     it("Fund and claim the funds", async function () {
-        const { campaign, creator, funder1, endDate } = await getCampaign();
+        const { campaign, creator, funder1, startDate, endDate } = await getCampaign();
         // fund campaign
         const fundingAmount = ethers.utils.parseUnits("0.01", "ether")
+        await time.increaseTo(startDate + 1000);
         await campaign.connect(funder1).donate({ value: fundingAmount });
         // check campaign balance
         expect(await campaign.getAmountCollected()).to.equal(fundingAmount);
         // check if goal is reached
         expect(await campaign.isGoalReached()).to.equal(true);
         // await end date
-        await time.increaseTo(endDate + 100);
+        await time.increaseTo(endDate + 1000);
         // claim funds
         await campaign.connect(creator).claimFunds();
     });
 
     it("Fund, not reach the goal and thus not claim the funds", async function () {
-        const { campaign, creator, funder1, endDate } = await getCampaign();
+        const { campaign, creator, funder1, endDate, startDate } = await getCampaign();
         // fund campaign
         const fundingAmount = ethers.utils.parseUnits("0.001", "ether")
+        await time.increaseTo(startDate + 1000);
         await campaign.connect(funder1).donate({ value: fundingAmount });
         // check campaign balance
         expect(await campaign.getAmountCollected()).to.equal(fundingAmount);
         // check if goal is reached
         expect(await campaign.isGoalReached()).to.equal(false);
         // await end date
-        await time.increaseTo(endDate + 100);
+        await time.increaseTo(endDate + 1000);
         // claim funds
         await expect(campaign.connect(creator).claimFunds()).to.be.revertedWith('The goal amount has not been reached.');
     });
@@ -50,7 +54,7 @@ describe("Campaign contract", function () {
         const { campaign, creator, funder1, startDate, endDate } = await getCampaign();
         // fund campaign
         const fundingAmount = ethers.utils.parseUnits("0.01", "ether")
-        await time.increaseTo(startDate + 100);
+        await time.increaseTo(startDate + 1000);
         await campaign.connect(funder1).donate({ value: fundingAmount });
         await expect(campaign.connect(creator).donate({ value: fundingAmount })).to.be.revertedWith('Owner cannot donate.');
         // check campaign balance
@@ -60,8 +64,8 @@ describe("Campaign contract", function () {
         // claim funds before end date
         await time.increaseTo(endDate - 1000);
         await expect(campaign.connect(creator).claimFunds()).to.be.revertedWith('The funding phase is still in progress.');
-        // await 1 second
-        await time.increaseTo(endDate + 100);
+        // await end date
+        await time.increaseTo(endDate + 1000);
         // claim funds
         await campaign.connect(creator).claimFunds();
     });
